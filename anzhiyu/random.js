1104e3f8758b50b833a6648465873b6c6aac39f9
c283782085 <jtcmq@vip.qq.com>
2025-02-25 17:25:04 +0800

    Site updated: 2025-02-25 17:25:03

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/02/21/v2ray/","2025/02/21/topsimple/","2025/02/21/教程/","2025/02/21/使用Acme.sh脚本申请证书/","2025/02/21/hello-world/"];function toRandomPost(){
+var posts=["2025/02/21/v2ray/","2025/02/21/topsimple/","2025/02/21/使用Acme.sh脚本申请证书/","2025/02/21/教程/","2025/02/21/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
